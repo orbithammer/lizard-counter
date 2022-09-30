f85997638053a36c0e7ee5f0f51833abd86fc3c6
Brock Penner <orbithammer@users.noreply.github.com>
2022-10-01 08:11:10 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-
+let countStr = ""
+    
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
 function save() {
-    let countStr = count + " - "
+    if (countStr.length === 0) {
+        countStr = count
+    } else {
+        countStr = " - " + count 
+    }
     saveEl.textContent += countStr
     count = 0
     countEl.textContent = count
